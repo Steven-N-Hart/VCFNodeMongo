bd3599873001e545071fa7c41018cb12ad234f00
Steven-N-Hart <Steven.N.Hart@gmail.com>
2015-06-18 23:29:22 -0500

    Worked a lot on export to get proper vcf

--- scripts/VCF_export.js
+++ scripts/VCF_export.js
@@ -47,8 +47,10 @@ var stream = fs.createWriteStream(output);
 
 
 // Split each study id and sample id and put quotes around them
-var studyIDs = studyID.split(",");
+var  studyIDs=studyID.split(',');
 
+//console.log('studyID='+ studyIDs.length+' '+studyIDs)
+//process.exit();
 
 //Step0.  Print header
 
@@ -58,13 +60,6 @@ getHeader(studyIDs);
 //Step3.  Print out the header
 //Step4.  Get the variants
 //Step5.  Print each variant row
-
-
-
-//process.exit();
-//Create basic object to use when outputing Samples in VCF
-var samplesObj = [];
-var SAMPLES = [];
 
 
 ///////////////////////////////////////
@@ -77,17 +72,21 @@ function getHeader(studyIDs){
 
 // Use connect method to connect to the Server to get the sample ids i need to look for
 function getSamples(studyIDs,cb){
+  var SAMPLES = [];
+
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     //console.log('Connected')
     var collection = db.collection('meta');
-       collection.find({'studyID' : {$in:studyIDs}},{'_id':0,'sample_id':1}).toArray(
+       collection.find({'studyID' : {$in: studyIDs}},{'_id':0,'sample_id':1}).toArray(
             function(err, documents) {
+              if (err){console.log('ERR='+err)}
+                //console.log('docs='+JSON.stringify(documents));
               for (var j=0; j<documents.length;j++){
-              //console.log('res='+documents[j].sample_id);
+              //console.log('j='+documents[j].sample_id);
               SAMPLES.push(documents[j].sample_id);
             }
-            //console.log('res='+SAMPLES);
+            //console.log('ARRAY='+SAMPLES);
             //db.close();
             printHeaderLine(db,SAMPLES,printVariants(SAMPLES,db))
             }
@@ -95,7 +94,7 @@ function getSamples(studyIDs,cb){
   });
 }
 
-
+var COUNT=0;
 
 
 function printHeaderLine(db,SAMPLES,callback) {
@@ -109,10 +108,8 @@ function printHeaderLine(db,SAMPLES,callback) {
   	};
 
 
-
-
-
 function printVariants(SAMPLES,db){
+  //console.log('COUNT='+COUNT);COUNT=COUNT+1;
 	var collection = db.collection('variants');
 	//console.log('studies = '+studies);
   	var cursor = collection.aggregate([
@@ -138,78 +135,82 @@ function printVariants(SAMPLES,db){
       }
       ]);
   	cursor.each(function(err,res){
-   		if(err){console.log('err = ' + err)}
- // process.exit();
-		var variants = res;
-		if(res !== null){
-   		//variants= JSON.parse(variants);
-   		var chrom = variants._id.chr;
-   		var pos = variants._id.pos;
-   		var ref = variants._id.ref;
-   		var alt = variants._id.alt;
-   		var FORMAT_FIELDS = {};
-   		var studyAN=0;
-   		var studyAC=0;
-   		//Get number of samples in study
-   		var len = variants.samples.length;
-   		//console.log('len = '+ len);
+      if(err){console.log('err = ' + err)}
+		  var variants = res;
+  		if(res !== null){
+     		//variants= JSON.parse(variants);
+     		var chrom = variants._id.chr;
+     		var pos = variants._id.pos;
+     		var ref = variants._id.ref;
+     		var alt = variants._id.alt;
+        var ARRAY = variants.samples;
+     		var FORMAT_FIELDS = {};
+     		var studyAN=0;
+     		var studyAC=0;
+     		//Get number of samples in study
+     		var len = SAMPLES.length;
 
-   		//loop over each sample results
-   		for (var whichSample=0;whichSample < len ;whichSample++){
-   			var SAMPLE = variants.samples[whichSample];
+     		//loop over each sample and get results
+     		for (var whichSample=0;whichSample < len ;whichSample++){
+     			var SAMPLE = SAMPLES[whichSample];
+          //console.log('ARRAY='+JSON.stringify(ARRAY))
 
-   			var a = studyIDs.indexOf(SAMPLE.study_id);
-   			//console.log('a='+a)
-   			//if a < 0, then the sample is not supposed to be exported
-   			if (a < 0){
-   				//console.log('This is not in query');
-   				break
-   			}
-        var sampleObj = new sampleData(SAMPLE.sample,SAMPLE.GT, SAMPLE.GQ, SAMPLE.HQ, SAMPLE.PL, SAMPLE.AD, SAMPLE.DP)
+          var a = ARRAY.map(function(e) { return e.sample }).indexOf(SAMPLE);
+     			//if a < 0, then the sample does not have data
+     			if (a < 0){
+     				//console.log('This is not in query');
+            var sampleObj = new sampleData(SAMPLE,'.','.', '.', '.', '.','.')
+     				//break
+     			}
+          else{
+            var sampleObj = new sampleData(ARRAY[a].sample,ARRAY[a].GT, ARRAY[a].GQ, ARRAY[a].HQ, ARRAY[a].PL, ARRAY[a].AD, ARRAY[a].DP)
+          }
+     	  //console.log('OBJ='+JSON.stringify(sampleObj))
+        //process.exit();
+      		//Calcuacate AC and AN
+     			if (sampleObj.GT === undefined || sampleObj.GT == './.'|| sampleObj.GT == '.' ){
+  	   			sampleObj.GT='.';
+  	   			studyAN++;studyAN++;
+  	   		}
+  	   	   	//Add 1 for heterozygous
+  	   		if (sampleObj.GT == '0/1' || sampleObj.GT == '0|1'||sampleObj.GT == '1/0' || sampleObj.GT == '1|0' ||sampleObj.GT == './1' ){
+  	   			studyAC++;studyAN++;studyAN++;
+  	   			//console.log('Added 1')
+  	   		}
+  	   		//Add 2 for homozygous
+  	   		if (sampleObj.GT == '1/1' || sampleObj.GT == '1|1'  ){
+  	   			studyAC++;
+  	   			studyAC++;studyAN++;studyAN++;
+  	   		}
+  	   		var formatData = sampleObj.GT+':'+sampleObj.AD+':'+sampleObj.DP+':'+sampleObj.GQ+':'+sampleObj.HQ;
+          //console.log(JSON.stringify(formatData))
+  	   		FORMAT_FIELDS[sampleObj.sample] = formatData ;
+        //console.log('JSON='+JSON.stringify(FORMAT_FIELDS))
+  	   	}//All samples have been evaluated
 
-   			//Calcuacate AC and AN
-   			if (sampleObj.GT === undefined || sampleObj.GT == './.'|| sampleObj.GT == '.' ){
-	   			sampleObj.GT='.';
-	   			studyAN++;studyAN++;
-	   		}
-	   	   	//Add 1 for heterozygous
-	   		if (sampleObj.GT == '0/1' || sampleObj.GT == '0|1'||sampleObj.GT == '1/0' || sampleObj.GT == '1|0' ||sampleObj.GT == './1' ){
-	   			studyAC++;studyAN++;studyAN++;
-	   			//console.log('Added 1')
-	   		}
-	   		//Add 2 for homozygous
-	   		if (sampleObj.GT == '1/1' || sampleObj.GT == '1|1'  ){
-	   			studyAC++;
-	   			studyAC++;studyAN++;studyAN++;
-	   		}
-	   		var formatData = sampleObj.GT+':'+sampleObj.AD+':'+sampleObj.DP+':'+sampleObj.GQ+':'+sampleObj.HQ;
-      //console.log(JSON.stringify(sampleObj))
-	   		FORMAT_FIELDS[sampleObj.sample] = formatData ;
+        //console.log(JSON.stringify(FORMAT_FIELDS))
+     		var studyAF=0;
+     		if (studyAC > 0 ){studyAF=studyAC/studyAN;}
+     		var INFO_LINE='AC='+studyAC+';AN='+studyAN+';AF='+studyAF+';';
+     		var FULL_LINE = [];
+        var GTarray=[];
+        for(var o in FORMAT_FIELDS) {
+            GTarray.push(FORMAT_FIELDS[o]);
+          }
+     		FORMAT_FIELDS=GTarray.join('\t');
+        //console.log('FORMAT_FIELDS='+FORMAT_FIELDS)
+        //console.log('INFO_LINE='+INFO_LINE)
 
-	   	}//All samples have been evaluated
-	   	console.log(JSON.stringify(FORMAT_FIELDS))
-  process.exit();
-//Still need to control for when samples dont have data!!!!
-
-
-
-
-
-
-   		//console.log('studyAC = '+ studyAC + ' StudyAN = ' + studyAN);
-   		var studyAF=0;
-   		if (studyAC > 0 ){studyAF=studyAC/studyAN;}
-   		var INFO_LINE='AC='+studyAC+';AN='+studyAN+';AF='+studyAF+';';
-   		var FULL_LINE = [];
-
-   		FORMAT_FIELDS=FORMAT_FIELDS.join('\t');
-   		FULL_LINE.push(chrom,pos,'.', ref,alt,'.','.',INFO_LINE,'GT:AD:DP:GQ:HQ',FORMAT_FIELDS);
-   		var fullLine = FULL_LINE.join('\t');
-   		stream.write(fullLine+'\n');
-   		console.log(fullLine)
-
-   	}
-   	});
+     		FULL_LINE.push(chrom,pos,'.', ref,alt,'.','.',INFO_LINE,'GT:AD:DP:GQ:HQ',FORMAT_FIELDS);
+     		var fullLine = FULL_LINE.join('\t');
+     		stream.write(fullLine+'\n');
+        console.log(fullLine)
+     	}//End if res is not null
+      else {
+        process.exit()
+      }
+   }//End Each
+  );
 }
 
 function printHeader(){
