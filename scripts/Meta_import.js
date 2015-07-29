98b1a642639d57847690916343ffa964ae2c9703
Raymond <raymond.moore301@gmail.com>
2015-07-29 17:32:13 -0500

    minor adjustment to meta, more error handling

--- scripts/Meta_import.js
+++ scripts/Meta_import.js
@@ -2,22 +2,21 @@
 /*jshint -W069 */
 'use strict';
 
-
 var CmdLineOpts = require('commander');
 var MongoClient = require('mongodb').MongoClient;
-var LineByLineReader = require('line-by-line');
 var assert = require('assert');
 var config = require('./config.json');
 var url = 'mongodb://' + config.db.host + ':' + config.db.port + '/' + config.db.name;
+var fs = require('fs');
+
 
 //Make sure are variables are set
-if(typeof process.argv[2] === 'undefined') {
-  console.log('Usage: Meta_import.js -i <tsv_file>');
-  console.log('\nMust contain "study_id", "sample_id", and "kit_id" or else it will fail');
-  console.log('\nAlso, the kit value needs to be registered before samples can be loaded');
-  process.exit();
+if (typeof process.argv[2] === 'undefined') {
+    console.log('Usage: Meta_import.js -i <tsv_file>');
+    console.log('\nMust contain "study_id", "sample_id", and "kit_id" or else it will fail');
+    console.log('\nAlso, the kit value needs to be registered before samples can be loaded');
+    process.exit();
 }
-
 
 
 /*
@@ -40,46 +39,47 @@ if (!CmdLineOpts.input) {
 }
 
 //Get the header and make sure the samples, study, and kit ids are set
-var fs = require('fs');
-var entireFile = fs.readFileSync(CmdLineOpts.input,'utf8');
-entireFile = entireFile.replace(/\r/g,'');
+var entireFile = fs.readFileSync(CmdLineOpts.input, 'utf8');
+entireFile = entireFile.replace(/\r/g, '');
 
 //lines is each line in the file
-var lines = entireFile.split('\n');
+var lines = entireFile.split(/\n/);
 var headerLine = [];
 //Strip out any character that isnt a letter or number
-lines[0]=lines[0].replace(/[^\w\s]/gi, '')
-headerLine = lines[0].split('\t');
+lines[0] = lines[0].replace(/[^\w\s]/gi, '');
+headerLine = lines[0].split(/\t/);
 
+if (lines.length === 1) {
+    console.log('meta file has only 1 line');
+    process.exit(2);
+}
 
 //check the file for required fields
 var sampleIndex = headerLine.indexOf('sample_id');
 var studyIndex = headerLine.indexOf('study_id');
 var kitIndex = headerLine.indexOf('kit_id');
 
-if (sampleIndex < 0 || studyIndex < 0 || kitIndex < 0){
-	console.log('you are missing a required value in your meta file');
-	console.log('sampleIndex='+sampleIndex);
-	console.log('studyIndex='+studyIndex);
-	console.log('kitIndex='+kitIndex);
-	process.exit(2);
+if (sampleIndex < 0 || studyIndex < 0 || kitIndex < 0) {
+    console.log('you are missing a required value in your meta file');
+    console.log('sampleIndex=' + sampleIndex + '  studyIndex=' + studyIndex + '  kitIndex=' + kitIndex);
+    process.exit(2);
 }
 
 //Check the sample is registered
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function (err, db) {
     assert.equal(null, err);
     //console.log('Connected')
-    for (var i=1; i<lines.length;i++){
+    for (var i=1; i<lines.length; i++){
     	var newline = lines[i].split('\t');
         //Make sure evey sample has the required metadata
-    	if (newline[sampleIndex] == null||newline[studyIndex] == null||newline[kitIndex] == null){
+    	if (newline[sampleIndex] === null||newline[studyIndex] === null||newline[kitIndex] === null){
     		console.log('you are missing a required value in your meta file');
 			console.log('sampleIndex='+sampleIndex);
 			console.log('studyIndex='+studyIndex);
 			console.log('kitIndex='+kitIndex);
 			process.exit(1);
     	}
-        console.log('NEWLINE2: '+ newline)
+        console.log('NEWLINE2: '+ newline);
     	// Make sure the kit is registered
         var PASS = validateKit(db, newline[kitIndex] );
    		//Make a query and insert string to more easily manage code
@@ -105,7 +105,7 @@ MongoClient.connect(url, function(err, db) {
    		);
     	}//end for i loop
     //db.close();
-    setTimeout(function(){db.close()}, 2000);
+    setTimeout(function(){db.close()}, 2000); ///// THIS IS REALLY HACKY!!!! Need to fix callback cascade.
 });
 
 function validateKit(db, kit ){
