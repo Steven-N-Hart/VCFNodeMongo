e729ee4c55d115a5379fb38c3075a107b418ac9c
Steven-N-Hart <steven.n.hart@gmail.com>
2016-07-28 14:06:22 -0500

    changed the stream pause to a more appropritate location

--- scripts/VCF_import.js
+++ scripts/VCF_import.js
@@ -13,6 +13,7 @@ var fs = require('fs');
 /*
  * Setup Commandline options, auto generates help
  */
+
 CmdLineOpts
     .version('0.0.1')
     .usage('[options] -i <file> -n studyname')
@@ -46,12 +47,12 @@ var sampleDbIds = [];
 var url = 'mongodb://' + config.db.host + ':' + config.db.port + '/' + config.db.name;
 MongoClient.connect(url, function (err, db) {
   assert.equal(null, err);
-  //readMyFileLineByLine(db, CmdLineOpts.input, function () {
+  // process the VCF file to get the header info 
   getSampleInfoFromFile(db, CmdLineOpts.input, function () {
-  // Time reporting - callback
+    // Now that I have the header info, let's start processing the data 
     getDataFromFile(db, CmdLineOpts.input);
     var ts2 = process.hrtime(ts1);
-    console.log('\n Total Time: %j s %j ms', ts2[0], (ts2[1] / 1000000));
+    console.log('Total Time: %j s %j ms', ts2[0], (ts2[1] / 1000000));
     //db.close();
   });
 });
@@ -68,6 +69,7 @@ var lineNum = 1;
 //Sychronus reading of file
 var getSampleInfoFromFile  = function (db, filepath, callback) {
   var lr = new LineByLineReader(filepath);
+  
   lr.on('error', function (err) {
     // 'err' contains error object
     console.log('Error reading file', err);
@@ -76,13 +78,13 @@ var getSampleInfoFromFile  = function (db, filepath, callback) {
   lr.on('line', function (line) {
     // 'line' contains the current line without the trailing newline character.
     if (line.match(/^#CHROM/)) {
-        //turn on indexing
-        var collection = db.collection(config.names.variant);
-        collection.ensureIndex("pos",function (){});
-        collection.ensureIndex("samples.sample_id",function (){});
+      //turn on indexing
+      var collection = db.collection(config.names.variant);
+      collection.ensureIndex("pos",function (){});
+      collection.ensureIndex("samples.sample_id",function (){});
       findSamples(db, line, function (ret) {
         sampleDbIds = ret;
-        lr.close(); //Stop reading file.  This caused memory leak
+        // go ahead and start exploring the header
         callback(db, filepath);
       });
     }
@@ -95,7 +97,8 @@ var getSampleInfoFromFile  = function (db, filepath, callback) {
 };
 
 
-
+//Get internal guts of the file
+//Asychronus reading of file
 var fs = require('fs')
   , util = require('util')
   , stream = require('stream')
@@ -106,35 +109,37 @@ var getDataFromFile  = function (db, filepath) {
     s = fs.createReadStream(filepath)
       .pipe(es.split())
       .pipe(es.mapSync(function (line) {
-       // pause the readstream - if the memory leak comes back, uncomment this and the resume, but it slows things down (3X)
-        //s.pause();
-
-
-        //lineNum += 1;
-
+      
         (function () {
             // process line here and call s.resume() when rdy
           if (!line.match(/^#/)) {
+             // pause the readstream - if the memory leak comes back, uncomment this and the resume, but it slows things down (3X)
+            s.pause();
             if(CmdLineOpts.newOnly){line=line.replace("0/0","./.")}
             processLines(line, lineNum, db, function () {
-                //console.log(lineNum)
+              lineNum+=1;
+                //console.log('lineNum=',lineNum,'len=',line.length,'line=',line);
+                //continue the stream
+               s.resume();
             });
           }
-          // resume the readstream
-          //s.resume(); //- if the memory leak comes back, uncomment this and the resume, but it slows things down
-        })();
+          if(line==''){
+           //console.log('Read entirefile.');
+            var ts2 = process.hrtime(ts1);
+            console.log('Total Time: %j s %j ms', ts2[0], (ts2[1] / 1000));
+            db.close()
+          }
+        })(
+
+        );
       })
         .on('error', function () {
           console.log('Error while reading file.');
         })
         .on('end', function () {
-
-          console.log('Read entirefile.');
-        var ts2 = process.hrtime(ts1);
-        console.log('\n Total Time: %j s %j ms', ts2[0], (ts2[1] / 1000000));
-        setTimeout(function(){ db.close(); }, 3000);   //THIS IS VERY HACKY AND WILL NOT ALWAYS WORK!
+          console.log('Done');
         })
-        );
+      ); // end pipe
   }
 
 /*###############################################################
@@ -147,12 +152,12 @@ var findSamples = function (db, ln, callback) {
     var chromLine = ln.split('\t');
     var sampleNames = chromLine.slice(9, chromLine.length); // no need for a loop.
     var collection = db.collection(config.names.sample);
-   //console.log({"study_id" : study_id, "sample_id" : { $in : sampleNames } });
 
     collection.find({"study_id" : study_id, "sample_id" : { $in : sampleNames } }).toArray(function (err, result) {
         //console.log('Array='+JSON.stringify(result))
+        // For each sample, make sure it is located in the database
         assert.equal(err, null, "Sample Check Issue");
-        // returns empty, if study wrong, or no samples exist
+        // returns empty if study wrong, or no samples exist
         if (result.length < 1) {
             console.error("No Sample(s) in this study. Need to register all samples to '" + study_id + "' first.");
             process.exit(5);
@@ -168,7 +173,7 @@ var findSamples = function (db, ln, callback) {
             for (var n in result) {
                 if( sampleNames[i] === result[n].sample_id ){
                     //console.log('ID='+result[n]._id.toHexString()+' n='+n+' i='+i)
-                    orderedSampleids[i] = result[n]._id.toHexString(); //COnverted to hex string so its easier to seach later
+                    orderedSampleids[i] = result[n]._id.toHexString(); //Converted to hex string so its easier to seach later
                     continue outerloop;  // small loop efficiency
                 }
             }
@@ -177,13 +182,10 @@ var findSamples = function (db, ln, callback) {
     });
 };
 
-
 //Process line
 var processLines = function (line,lineNum, db, callback) {
     VariantRecord.parseVCFline(line, function(myVar){
         // file line is parsed into an object, now do database work
-        //console.log('myVar='+JSON.stringify(myVar))
-        //console.log(linediff)
         findVariant(myVar, db, function(ret) {
             updateVariant(myVar, ret, db, lineNum,function() {
                 callback();
@@ -192,54 +194,43 @@ var processLines = function (line,lineNum, db, callback) {
     });
 };
 
-
-
 var findVariant = function(varObj, db, callback) {
     var collection = db.collection(config.names.variant);
-    //console.log('varObj.variant='+JSON.stringify(varObj.variant));   //varObj.variant,
-   // var updateableVar = varObj.variant;
-    //updateableVar['$setOnInsert'] = {needsAnnotation: true}; // {$setOnInsert: {needsAnnotation: true}}
-    //console.log('varObj.variant='+JSON.stringify(varObj.variant));
 
    collection.findOneAndUpdate(varObj.variant, {$setOnInsert: {needsAnnotation: true}}, {upsert:true, returnOriginal:true}, function(err, found) {
         assert.equal(err, null);
+        numbInserted +=1
         if (found.lastErrorObject.updatedExisting){
-            //collection.update({_id:found.value._id, needsAnnotation: true}, function(err, fnd) {
-                 //console.log("NEW! ",found.value);
                 //This entry has been seen before
                 callback({_id:found.value._id});
-            //});
         }
         else{
             //This particular variant has not been seen before
-            //console.log("Old! ",found.value);
             callback( {_id:found.lastErrorObject.upserted} );
         }
     });
 };
 
-
-
-
 var updateVariant = function(varObj, retVariant, db, lineNum,callback){
     var collection = db.collection(config.names.variant);
     var allSamples = [];// retVariant;
-    //console.log('retVariant: '+JSON.stringify(retVariant)) //retVariant: {"_id":"55b1afbe3daa7a9f4c443850"}
+
     for (var h in varObj.sampleFormats){
         if (varObj.sampleFormats[h] === null){ continue; } //skip samples that don't carry this variant
         varObj.sampleFormats[h]['sample_id'] = sampleDbIds[h];
         allSamples.push(varObj.sampleFormats[h]);
     }
+
     //console.log('All Samples: '+ JSON.stringify(allSamples)) //[{"GT":"0|1","GQ":48,"DP":8,"HQ":[51,51],"GTC":1,"sample_id":"559a9bb5efeea832eafa8520"},{"GT":"0/1","GQ":43,"DP":5,"HQ":[null,null],"GTC":1,"sample_id":"559a9bb5efeea832eafa8521"}]
-    /// This makes one query, updates any changes & inserts anything new
-    //only load if there is a smple with a variant
+    /// This makes one query that updates any changes & inserts anything new
+    // only load if there is a smple with a variant
 
     collection.update(retVariant,{$pushAll:{samples:allSamples}},{upsert:true,safe:false}, function (err,data) {
     if (err){ console.error(err); process.exit()}
     else{
-        //numbInserted++;
-        //linediff=lineNum - numbInserted
-        //console.log("Var " + varObj.variant.chr + ":" + varObj.variant.pos + " " + varObj.variant.ref + ">" + varObj.variant.alt + "\tS=" + allSamples.length,'\tInserted='+numbInserted+'\tOn Line ='+lineNum,'\tDifference='+linediff);
+        if(!data){
+          console.log('eof')
+        }
     }
     callback();
     });
